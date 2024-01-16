a1fb6adb6647b4090d7638b0d7c02ede5c2bc685
dgavero <dtopdave1997@gmail.com>
2024-01-16 17:47:54 +0800

    Login user automation

--- app_test_cases/test_login_user.py
+++ app_test_cases/test_login_user.py
@@ -4,13 +4,9 @@ from app_test_cases.BaseTest import BaseTest
 from app_utilities import dataProvider
 
 
-class Test_Technicianlogin(BaseTest):
+class Test_UserLogin(BaseTest):
 
     @pytest.mark.parametrize("username,password", dataProvider.get_data("login_credentials"))
-    def test_techlogins(self, username, password):
+    def test_userlogin(self, username, password):
         login = LoginScreen(self.driver)
-        login.techLogin(username, password)
-
-
-
-
+        login.userLogin(username, password)
